f07e3d663910bd176787f3e96e03f558569c1ff0
Andrew Ashworth <andrew@ashworth.us>
2025-05-18 18:24:56 -0400

    initial, non-working ispc bindings

--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -1,5 +1,7 @@
 #include <nanobind/nanobind.h>
 
+#include <mandelbrot_ispc.h>
+
 namespace nb = nanobind;
 
 using namespace nb::literals;
@@ -7,4 +9,5 @@ using namespace nb::literals;
 NB_MODULE(nanobind_example_ext, m) {
     m.doc() = "This is a \"hello world\" example with nanobind";
     m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
+    m.def("mandelbrot_ispc", &ispc::mandelbrot_ispc);
 }
